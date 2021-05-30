a8db89090267c2665ead11cc7d7e3f1ec04c5a24
mrvirusrecord <81562689+mrvirusrecord@users.noreply.github.com>
2021-05-30 20:50:46 +0700

    Add files via upload

---
+++ gipwy.sh
@@ -0,0 +1,194 @@
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo
+echo $green
+    figlet MR VIRUS
+echo
+sleep 1
+  echo $blue"=========================================="
+  echo $purple"(+) AUTHOR : MR VIRUS SPM              (+)"
+  echo "(+) TEAM   : MAVIA TEKNOLOGI INDONESIA (+)"
+  echo "(+) KONTAK : +62 882-1235-2864         (+)"
+  echo $blue"=========================================="
+sleep 2
+      echo $yellow
+      echo
+          read -p "MASUKKAN PASSWORD DULU NGAB : " trey
+if [ $trey = 89760 ]
+then
+clear
+    sleep 2
+echo $white
+   sleep 0
+        echo "+62 831-9592-6669"
+sleep 0
+        echo "+62 853-4171-7106"
+sleep 0
+        echo "+62 812-8612-2979"
+sleep 0
+        echo "+62 856-0053-6814"
+sleep 0
+        echo "+62 858-0830-3642"
+sleep 0
+        echo "+62 858-5059-9149"
+sleep 0
+        echo "+62 857-3336-3979"
+sleep 0
+        echo "+62 852-3617-0356"
+sleep 0
+        echo "+62 821-1842-6491"
+sleep 0
+        echo "+62 857-7917-5576"
+sleep 0
+        echo "+62 881-0371-00206"
+sleep 0
+        echo "+62 821-6711-7720"
+sleep 0
+        echo "+62 821-4078-9230"
+sleep 0
+        echo "+62 822-7415-8207"
+sleep 0
+        echo "+62 812-2576-7437"
+sleep 0
+        echo "+62 813-4549-4791"
+sleep 0
+        echo "+62 831-0448-6267"
+sleep 0
+        echo "+62 822-7757-6785"
+sleep 0
+        echo "+62 852-9871-1321"
+sleep 0
+        echo "+62 821-7169-4128"
+sleep 0
+        echo "+62 877-8562-7762"
+sleep 0
+        echo "+62 896-5319-2287"
+sleep 0
+        echo "+62 852-3060-9196"
+sleep 0
+        echo "+62 822-8462-3351"
+sleep 0
+        echo "+62 815-2572-3170"
+sleep 0
+        echo "+62 882-6810-0243"
+sleep 0
+        echo "+62 821-8152-5846"
+sleep 0
+        echo "+62 895-3363-91063"
+sleep 0
+        echo "+62 857-9418-8178"
+sleep 0
+        echo "+62 859-1804-11835"
+sleep 0
+        echo "+62 831-2870-0003"
+sleep 0
+        echo "+62 852-8154-8798"
+sleep 0
+        echo "+62 895-3203-85608"
+sleep 0
+        echo "+62 881-7603-107"
+sleep 0
+        echo "+62 813-9175-6710"
+sleep 0
+        echo "+62 882-1697-3416"
+sleep 0
+        echo "+62 823-0129-8429"
+sleep 0
+        echo "+62 878-3712-4666"
+sleep 0
+        echo "+63 821-7172-4223"
+sleep 0
+        echo "0852-6697-0204"
+sleep 0
+        echo "0857-7267-6495"
+sleep 0
+        echo "0819-4770-2853"
+sleep 0
+        echo "0858-8849-7547"
+sleep 0
+        echo "0813-4710-7033"
+sleep 0
+        echo "+62 813-8201-1190"
+sleep 0
+        echo "0896-2807-7388"
+sleep 0
+        echo "085335019580"
+sleep 0
+        echo "082258896755"
+sleep 0
+        echo "085732055041"
+sleep 0
+        echo "082394542812"
+sleep 0
+        echo "085314268899"
+sleep 0
+        echo "083112834549"
+sleep 0
+        echo "082350390359"
+sleep 0
+        echo "Vina : 085314266840"
+sleep 0
+        echo "Siswani : 082311455485"
+sleep 0
+        echo "Lidya : 083822186229"
+sleep 0
+        echo "Ismi : 083832180426"
+sleep 0
+        echo "Ayudina : 082317272520"
+sleep 0
+        echo "Salma : 085256034294"
+sleep 0
+        echo "Widya : 082317277118"
+sleep 0
+        echo "Jessica : 085324386679"
+sleep 0
+        echo "Nur : 082311455485"
+sleep 0
+        echo "Carina : 085321606820"
+sleep 0
+        echo "Agita : 085321606820"
+sleep 0
+        echo "Putry : 083898077499"
+sleep 0
+        echo "Yenika : 082187748875"
+sleep 0
+        echo "Natalia : 082311455485"
+sleep 0
+        echo "Salbina : 085242557186"
+sleep 0
+        echo "Sherlina : 085315470011"
+sleep 0
+        echo "Fitra : 087772216729"
+sleep 0
+        echo "Nifa : 0895335180271"
+sleep 0
+        echo "Diana : 082317277118"
+sleep 0
+        echo "Ayu Nawal : 082316538570"
+sleep 0
+        echo "Puput : 085813436735"
+sleep 0
+        echo "Yuvita : 085335076294"
+sleep 0
+        echo "Novita Dewi : 085335019635"
+sleep 0
+        echo "Beby Riska (089523641376)"
+sleep 0
+        echo "Vinna (085768313819)"
+sleep 0
+        echo "Yunita (083822159029)"
+sleep 0
+        echo "Melisa (083843069913)"
+sleep 0
+        echo "Christin (089675839108)"
+sleep 1
+echo
+fi
